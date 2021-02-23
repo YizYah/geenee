2cbf45e588396cdb0b16c6659a8afd41043b379e
YizYah <yyakovson613@gmail.com>
2021-02-23 18:22:07 +0200

    added head-starter

--- src/custom/generate/createCodeBase.ts
+++ src/custom/generate/createCodeBase.ts
@@ -1,12 +1,18 @@
 import {createNewCode} from './createNewCode'
 
-const {commands, dirNames, fileNames, docPages, links, suffixes} = require('magicalstrings').constants
-
+import {Configuration, CustomCodeRepository, NsInfo} from 'magicalstrings'
 import {copyTemplateToMeta} from './copyTemplateToMeta'
-import {createStarter} from '../headStart/createStarter'
 
+const getConfig = require('magicalstrings').configs.getConfig
+const {commands, dirNames, fileNames, docPages, links, suffixes} = require('magicalstrings').constants
+const {dirOptions} = require('magicalstrings').dirOptions
+const setNsInfo = require('magicalstrings').nsFiles.setNsInfo
+
+// import {createStarter} from '../headStart/createStarter'
+const createStarter = require('head-starter')
 const generateCode = require('geenee-spell')
 const fs = require('fs-extra')
+const yaml = require('js-yaml')
 
 export async function createCodeBase(
   templateDir: string | undefined,
@@ -51,9 +57,44 @@ export async function createCodeBase(
   }
 
   if (templateDir && (!existsCodeTemplateDir || !noSetup)) {
+    const config: Configuration = await getConfig(templateDir)
+    const {setupSequence} = config
     await createStarter(
-      templateDir, codeDir, session
+      setupSequence, codeDir, session
     )
+
+    let nsInfo: NsInfo
+    try {
+      const nsYml = fs.readFileSync(`${templateDir}/${fileNames.SAMPLE_NS_FILE}`, 'utf8')
+      nsInfo = await yaml.safeLoad(nsYml)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(`error opening sample ns file in the template directory ${templateDir}`)
+      throw error
+    }
+
+    const starterDir = codeDir + suffixes.STARTUP_DIR
+    const metaDir = `${starterDir}/${dirNames.META}`
+    const customCode = `${metaDir}/${fileNames.CUSTOM_CODE_FILE}`
+    if (nsInfo) nsInfo.starter = starterDir
+
+    const customDir = `${starterDir}/${config.dirs.custom}`
+    const customCodeRepository: CustomCodeRepository = {
+      addedCode: {},
+      replacedCode: {},
+      removedCode: {},
+    }
+
+    try {
+      await fs.ensureDir(metaDir, dirOptions)
+      await fs.ensureDir(customDir, dirOptions)
+      await setNsInfo(starterDir, nsInfo)
+      await fs.outputJson(customCode, customCodeRepository)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+
     const nsFilePath = `${codeDir}/${dirNames.META}/${fileNames.NS_FILE}`
     if (!await fs.pathExists(nsFilePath)) {
       // if the settings file doesn't exist yet then it's a brand new Template...
